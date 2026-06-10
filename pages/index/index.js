c179ace564b5c2720d9a0988231fbdde1a97b07b
crowww1113 <i057647601x@163.com>
2026-06-10 16:04:23 +0800

    首页开发

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,2 +1,32 @@
-// index.js
-Page({})
+Page({
+  data: {
+    username: '',
+    realName: '',
+    roleName: ''
+  },
+
+  onShow() {
+    this.loadUserInfo();
+  },
+
+  // 读取用户信息
+  loadUserInfo() {
+    const userInfo = wx.getStorageSync('userInfo');
+    if (userInfo) {
+      this.setData({
+        username: userInfo.username || '',
+        realName: userInfo.realName || userInfo.username || '',
+        roleName: userInfo.roleName || ''
+      });
+    }
+  },
+
+  // 点击功能块
+  onTapGrid(e) {
+    const type = e.currentTarget.dataset.type;
+    wx.showToast({
+      title: '功能开发中',
+      icon: 'none'
+    });
+  }
+});
